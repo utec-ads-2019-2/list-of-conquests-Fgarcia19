0e3bde55d7bacf9ecf6959325ab87d1df7f4d199
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2019-08-25 17:59:59 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+
+void imprimir(map<string,int> map){
+
+    for(auto it:map){
+
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+
+}
+
+void lista(int lines) {
+    map<string, int> repeticiones_paises;
+    string persona;
+    string pais;
+
+    for (int i = 0; i < lines; i++) {
+        cin >> pais;
+        getline(cin, persona);
+        if (repeticiones_paises.find(pais) != repeticiones_paises.end()) {
+
+            repeticiones_paises[pais]++;
+        } else {
+            repeticiones_paises.insert(pair<string, int>(pais, 1));
+
+        }
+
+
+    }
+    imprimir(repeticiones_paises);
+}
+
+
+int main() {
+    int lines;
+    cin>>lines;
+    lista(lines);
     return 0;
 }
+
